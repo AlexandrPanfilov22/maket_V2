5b9fec58378408e55d6d3ce0afd2cbb9ee35cb07
Александр Панфилов <panfilove4516@gmail.com>
2023-03-23 13:58:30 +0300

    feature (scripts): change header toggler and change add new card

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,55 @@
 const burgerBtn = document.querySelector('.menu-button');
 const menu = document.querySelector('.menu');
-let show = false;
 
 const services = document.querySelector('.services-card-container');
-const addCardButton = document.querySelector('.add-card');
-const card = document.querySelector('.services-card-container');
-const cloneCard = card.cloneNode(true);
+const addBtn = document.querySelector('.add-card');
 
-function togler() {
-    if(show) {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-
-    show = !show;
+function toggleMenu() {
+    menu.classList.toggle('menu');
 }
-burgerBtn.addEventListener('click', togler);
+burgerBtn.addEventListener('click', toggleMenu);
 
 function addNewCard() {
-    services.append(cloneCard);
+    const cardContainer = document.createElement('div');
+    const card = document.createElement('div');
+    const imageContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const titleContainer = document.createElement('h3');
+    const title = document.createElement('span');
+    const inTitle = document.createElement('span');
+    const description = document.createElement('p');
+    const link = document.createElement('a');
+
+    cardContainer.className = 'col-12 col-sm-7 col-md-6 col-xl-4 text-xl-start text-center mb-5';
+    card.className = 'services-card mb-5 mb-xl-0';
+
+    imageContainer.className = 'services-card mb-5 mb-xl-0';
+    image.className = 'services-card mb-5 mb-xl-0';
+    image.setAttribute('src', 'img/office.png');
+
+    titleContainer.className = 'title';
+    title.className = 'title';
+    title.innerHTML = 'Исследование и упаковка';
+
+    inTitle.className = 'text-primary';
+    inTitle.innerHTML = '01.';
+
+    description.className = 'description card-description';
+    description.innerHTML = 'Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.'
+
+    link.className = 'link d-block';
+    link.innerHTML = 'Подробнее';
+
+    cardContainer.append(card);
+    card.append(imageContainer);
+    imageContainer.append(image);
+    card.append(titleContainer);
+    titleContainer.append(inTitle);
+    titleContainer.append(title);
+    card.append(description);
+    card.append(link);
+
+    services.append(cardContainer);
 }
 
-addCardButton.addEventListener('click', addNewCard);
+addBtn.addEventListener('click', addNewCard);
